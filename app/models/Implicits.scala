7321c7478649308995d975689afd762c32c87b93
nemoo <jokap@yahoo.com>
2024-04-29 12:02:20 +0200

    add type info

--- app/models/Implicits.scala
+++ app/models/Implicits.scala
@@ -1,8 +1,10 @@
 package models
 
 import com.github.takezoe.slick.blocking.BlockingPostgresDriver.blockingApi._
+import slick.ast.BaseTypedType
+import slick.jdbc.JdbcType
 
 object Implicits{
-  implicit val taskStatusColumnType = MappedColumnType.base[TaskStatus.Value, String](
+  implicit val taskStatusColumnType: JdbcType[TaskStatus.Value] with BaseTypedType[TaskStatus.Value] = MappedColumnType.base[TaskStatus.Value, String](
     _.toString, string => TaskStatus.withName(string))
 }
